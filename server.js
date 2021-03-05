559df278f0147694eb9c474429f2021e900b2ad4
William Massin <43667115+Dunthil@users.noreply.github.com>
2021-03-05 15:14:18 +0100

    user name

---
+++ server.js
@@ -0,0 +1,43 @@
+//On load les modules
+const express = require('express');
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 3000;
+const index = '/index.html';
+
+//on setup le serv
+const server = express() .use((req, res)=> {
+
+    res.sendFile(index, { root: __dirname })
+
+})
+.listen(port, () =>{
+    console.log(`Server started on port: ${port}`);
+})
+
+//on setup socket
+const io = socketIO(server);
+
+let users = [];
+
+io.on('connection', (socket) => {
+
+    socket.on('username', (username) =>{
+        console.log(`${username} joined the game.`);
+
+        socket.username = username;
+
+        users.push(socket);
+    });
+
+});
+
+function sendUsers() {
+    const usersData = users.map((user) =>{
+
+        return {
+            username: user.username
+        }
+
+    });
+}
